609cd38300b1bd624f590d178a9e51f78818c5bd
Jake Delaney <76518393+JakeDelaney@users.noreply.github.com>
2026-04-25 15:57:50 +0100

    Style navbar
    Add bootstrap classes to style navbar, child and descendant elements

--- templates/navbar.php
+++ templates/navbar.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-expand bg-success">
     <div class="container-fluid">
-    <h1 class="text-light">Emerald Records</h1>
-        <div class="navbar-nav">
-            <a class="nav-link text-light active" href="index.php">Home</a>
-            <a class="nav-link text-light" href="">About</a>
-            <a class="nav-link text-light" href="">Products</a>
-            <a class="nav-link text-light" href="">Contact</a>
-            <a class="nav-link text-light" href="">Account</a>
+    <h1 class="text-light ms-3">Emerald Records</h1>
+        <div class="navbar-nav h4">
+            <a class="nav-link text-light active mx-3" href="index.php">Home</a>
+            <a class="nav-link text-light mx-3" href="">About</a>
+            <a class="nav-link text-light mx-3" href="">Products</a>
+            <a class="nav-link text-light mx-3" href="">Contact</a>
+            <a class="nav-link text-light mx-3" href="">Account</a>
         </div>
     </div>
 </nav>
