21f9b4f8440197c9c5964c3d74b39c2a1dcab585
Jake Delaney <76518393+JakeDelaney@users.noreply.github.com>
2026-04-25 15:46:45 +0100

    Add navbar template
    Adds navigation bar template and placeholders for navigate links

--- public/index.php
+++ public/index.php
@@ -1,10 +1,14 @@
 <?php
-require("../templates/header.php");
+require_once("../templates/header.php");
 ?>
     
     <title>Home page</title>
 </head>
 <body>
+    <?php
+    require_once("../templates/navbar.php");
+    ?>
+
     <h1>Welcome to the home page!</h1>
 </body>
 </html>
---
+++ templates/navbar.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-expand bg-success">
+    <div class="container-fluid">
+    <h1 class="text-light">Emerald Records</h1>
+        <div class="navbar-nav">
+            <a class="nav-link text-light active" href="index.php">Home</a>
+            <a class="nav-link text-light" href="">About</a>
+            <a class="nav-link text-light" href="">Products</a>
+            <a class="nav-link text-light" href="">Contact</a>
+            <a class="nav-link text-light" href="">Account</a>
+        </div>
+    </div>
+</nav>
