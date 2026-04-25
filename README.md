61d4c0851a53ea6086229a1a3326510cdf4356b4
Jake Delaney <76518393+JakeDelaney@users.noreply.github.com>
2026-04-25 16:35:06 +0100

    Add content to index.php
    Add text placeholder content and image to index.php, style img border using native css

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # PHP-eCommerce-Project
 E-Commerce PHP website with CRUD and sessions functionality
+
+# Images
+All images used within this site are royalty free. Information on each image used can be found referenced below, and cross-referenced within the website through their HTML <alt> tags:
+
+"Two men browsing a display of vinyl records"
+[Accessed from:](https://www.pexels.com/photo/vintage-vinyl-enthusiasts-in-record-store-33003441/)
+
+# Tools
+[Adobe Image Resizer](https://www.adobe.com/express/feature/image/resize)
+*Used for resizing all images within the website
+
+
--- public/index.php
+++ public/index.php
@@ -9,6 +9,22 @@ require_once("../templates/header.php");
     require_once("../templates/navbar.php");
     ?>
 
-    <h1>Welcome to the home page!</h1>
+    <div class="container mt-4 text-center">
+        <div class="row">
+            <h2>Emerald Records - Ireland's Premier Vinyl Shop!</h2>
+            <hr>
+        </div>
+        <div class="row mt-4">
+            <div class="col">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo aspernatur reiciendis autem quisquam officiis cumque laborum! Fugiat modi veritatis laborum, debitis eius vero incidunt possimus inventore, ducimus cupiditate, at veniam.
+                Reiciendis ipsum quaerat tenetur quisquam saepe cum debitis corrupti, blanditiis architecto explicabo, sequi magni repudiandae praesentium cumque nostrum recusandae unde, quia nobis commodi veritatis harum officia ex impedit! Voluptates, error!</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus nobis doloribus voluptas mollitia delectus necessitatibus quas alias veniam repellendus quia sed rerum, veritatis harum in dignissimos aliquam facilis officia. Ratione!
+                Rem ea quia optio delectus magni magnam consequuntur explicabo fugiat unde non deleniti iusto accusantium odio culpa corporis animi blanditiis incidunt, aperiam maxime molestiae numquam eveniet natus vitae? Earum, saepe!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium suscipit, quos quidem quisquam ab inventore, esse consequatur voluptatibus ipsum delectus, minima illum! Voluptatibus, adipisci dolor? Recusandae necessitatibus sapiente doloremque maiores!</p>
+            </div>
+            <div class="col">
+                <img src="../assets/img/store1.jpg" alt="Two men browsing a display of vinyl records">
+        </div>
+    </div>
 </body>
 </html>
