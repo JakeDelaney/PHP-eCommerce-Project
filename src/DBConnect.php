60697e6fec14be3ceec282d9d1e4010937a71f86
Jake Delaney <76518393+JakeDelaney@users.noreply.github.com>
2026-04-26 16:22:02 +0100

    Create DBConnect.php file
    DBConnect contains an instance from the PDO class, and is responsible for establishing the database connection

---
+++ src/DBConnect.php
@@ -0,0 +1,10 @@
+<?php
+//Retrieves database credentials from config file
+require_once '../public/config.php';
+//Creates an instance of PDO class and attempts to open database connection with values from config.php passed as arguments
+try {
+    $connection = new PDO($dsn, $username, $password, $options);
+} catch (PDOException $error) {
+    throw new PDOException($error->getMessage(), (int)$error->getCode());
+}
+?>
