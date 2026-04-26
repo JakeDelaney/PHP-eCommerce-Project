66c00587f1a0da4bbe4971dd8f598e610172cafa
Jake Delaney <76518393+JakeDelaney@users.noreply.github.com>
2026-04-26 12:44:31 +0100

    Update styling to signup form
    Updates styling to signup form, moves buttons outside of form and links through id

--- public/signup.php
+++ public/signup.php
@@ -11,32 +11,40 @@ require_once("../templates/header.php");
 
     <div class="container mt-4 text-center">
         <div class="row">
-            <h2>Sign up - necome a member!</h2>
+            <h2>Sign up - become a member!</h2>
             <hr>
         </div>
-        <div class="row mt-4 signup-form ">
-            <form action="" class="bg-dark">
-                <div class="form-floating">
-                    <input type="text" class="form-control" id="floatingUserName" placeholder="JohnDeer007">
-                    <label for="floatingUserName">Username</label>
-                </div>
-                <div class="form-floating mt-2">
-                    <input type="email" class="form-control" id="floatingEmail" placeholder="name@example.com">
-                    <label for="floatingEmail">Email</label>
-                </div>
-                <div class="form-floating mt-2">
-                    <input type="text" class="form-control" id="floatingAddress" placeholder="37 Make Believe Avenue">
-                    <label for="floatingAddress">Address</label>
-                </div>
-                <div class="form-floating mt-2">
-                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                    <label for="floatingUPassword">Password</label>
-                </div>
-                <div class="form-floating mt-2">
-                    <input type="password" class="form-control" id="floatingPasswordConfirm" placeholder="Confirm password">
-                    <label for="floatingPasswordConfirm">Confirm password</label>
-                </div>
-            </form>
+        <div class="row mt-4">
+            <div class="col-4 mx-auto signup-form-container p-3 gap-4">
+                <form action="" id="signup-form">
+                    <div class="form-floating">
+                        <input type="text" class="form-control" id="floatingUserName" placeholder="JohnDeer007">
+                        <label for="floatingUserName">Username</label>
+                    </div>
+                    <div class="form-floating mt-3">
+                        <input type="email" class="form-control" id="floatingEmail" placeholder="name@example.com">
+                        <label for="floatingEmail">Email</label>
+                    </div>
+                    <div class="form-floating mt-3">
+                        <input type="text" class="form-control" id="floatingAddress" placeholder="37 Make Believe Avenue">
+                        <label for="floatingAddress">Address</label>
+                    </div>
+                    <div class="form-floating mt-3">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                        <label for="floatingUPassword">Password</label>
+                    </div>
+                    <div class="form-floating mt-3">
+                        <input type="password" class="form-control" id="floatingPasswordConfirm" placeholder="Confirm password">
+                        <label for="floatingPasswordConfirm">Confirm password</label>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col">
+                <button type="submit" form="signup-form" class="btn btn-primary me-2">Submit</button>
+                <button type="reset" form="signup-form" class="btn btn-secondary ms-2">Reset</button>
+            </div>
         </div>
     </div>
 <?php
