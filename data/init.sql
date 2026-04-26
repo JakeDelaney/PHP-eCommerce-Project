ea6621312d3677bcbb72a7ac54569b70c5b077fb
Jake Delaney <76518393+JakeDelaney@users.noreply.github.com>
2026-04-26 14:54:12 +0100

    Create init.sql file
    Create init.sql file to initialize database with users and products table
    
    Inserts values into products table

---
+++ data/init.sql
@@ -0,0 +1,26 @@
+DROP DATABASE emerald_records_db;
+CREATE DATABASE emerald_records_db;
+USE emerald_records_db;
+
+CREATE TABLE users (
+    user_id INT UNSIGNED AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    username VARCHAR(20) UNIQUE NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE products (
+    product_id INT UNSIGNED AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    product_name VARCHAR(50) UNIQUE NOT NULL,
+    artist_name VARCHAR(50) NOT NULL,
+    music_genre VARCHAR(20),
+    year_released YEAR,
+    quantity int
+);
+
+INSERT INTO products (product_name, artist_name, music_genre, year_released, quantity)
+VALUES ('Pixie Dust - Single', 'Baelor Swift', 'Pop', 2025, 50),
+       ('Acid Death - LP', 'Non-Metallica', 'Metal', 1990, 50),
+       ('Cherry Fields - Single', 'Nuns N Noses', 'Rock', 1982, 50),
+       ('The Grey Album - Double-sided LP', 'The Bees', 'Rock', 1968, 50),
+       ('Horizon - Single', 'Draft Funk', 'Electronic', 2004, 50);
