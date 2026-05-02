b810d502f7f7bfe9c6c385c84d36982c9d22ecd8
JakeDelaney <jakedelaney24@gmail.com>
2026-05-02 18:43:19 +0100

    Create session-start.php
    Creates a central location for other PHP pages to participate in a PHP session, by requiring this file and its function

---
+++ src/session-start.php
@@ -0,0 +1,5 @@
+<?php
+//This file is a central location to handle the initiation of PHP sessions
+//Any PHP page requiring session functionality can do so by requiring this file
+session_start();
+?>
