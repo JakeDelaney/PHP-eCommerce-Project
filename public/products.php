c7abe049daf6122a0c5fda11e4d3852ab6cef9f4
JakeDelaney <jakedelaney24@gmail.com>
2026-05-03 10:47:05 +0100

    Update products.php
    Updates products file with logic to query DB and retrieve list of all available products
    
    List is retrieved and formatted as a HTML table

--- public/products.php
+++ public/products.php
@@ -1,7 +1,20 @@
 <?php
 require_once("../templates/header-guest.php");
+require_once("../src/sanitize.php")
 ?>
-    
+
+<?php
+    try {
+        require_once '../src/DBconnect.php';
+        $sql = "SELECT * FROM products";
+        $statement = $connection->prepare($sql);
+        $statement->execute();
+        $result = $statement->fetchAll();
+    } catch(PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
+?>
+
 <title>Products page</title>
 </head>
 <body>
@@ -10,10 +23,33 @@ require_once("../templates/header-guest.php");
     ?>
 
     <!--MAIN CONTAINER-->
-    <div class="container-fluid text-center">
+    <div class="container mt-4 text-center">
+        <div class="row">
+            
+        </div>
         <h2>Products</h2>
-        <table>
-            Table will go here
+        <hr>
+        <table class="table table-striped mt-4">
+            <thead class="table-success">
+                <tr>
+                    <th class="col-3">Record name</th>
+                    <th class="col">Artist</th>
+                    <th class="col">Genre</th>
+                    <th class="col">Released</th>
+                    <th class="col">Quantity</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($result as $row) { ?>
+                <tr>
+                    <td><?php echo sanitize($row["product_name"]); ?></td>
+                    <td><?php echo sanitize($row["artist_name"]); ?></td>
+                    <td><?php echo sanitize($row["music_genre"]); ?></td>
+                    <td><?php echo sanitize($row["year_released"]); ?></td>
+                    <td><?php echo sanitize($row["quantity"]); ?></td>
+                </tr>
+                <?php } ?>
+            </tbody>
         </table>
     </div>
 <?php
